678ccdcdb7e2eddbdc047552190649d7cadd468d
Avinash Bharti <avinashbharti3007@gmail.com>
2026-05-15 21:59:27 +0530

    Time: 0 ms (100%), Space: 14.3 MB (10.85%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,8 +1,32 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
-        priority_queue<int,vector<int>,greater<int>>pq;
-        for(int x : nums) pq.push(x);
-        return pq.top();
+    int findMin(vector<int>& arr) {
+        int lo = 0;
+        int hi = arr.size() - 1;
+        int ans = INT_MAX;
+
+        while (lo <= hi) {
+
+            // Agar subarray already sorted hai
+            if (arr[lo] <= arr[hi]) {
+                ans = min(ans, arr[lo]);
+                break;
+            }
+
+            int mid = lo + (hi - lo) / 2;
+
+            // Left part sorted
+            if (arr[lo] <= arr[mid]) {
+                ans = min(ans, arr[lo]);
+                lo = mid + 1;
+            }
+            // Right part sorted
+            else {
+                ans = min(ans, arr[mid]);
+                hi = mid - 1;
+            }
+        }
+
+        return ans;
     }
 };
