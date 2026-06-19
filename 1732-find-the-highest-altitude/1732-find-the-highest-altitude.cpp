6f2a06f97ac043a42ce8e2beeca77106e680c387
Avinash Bharti <avinashbharti3007@gmail.com>
2026-06-19 21:56:48 +0530

    Time: 11 ms (0.76%), Space: 10.8 MB (44.68%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int n = gain.size();
+        if(n==1) {
+            if(gain[0]<0) return 0;
+            else return gain[0];
+        }
+        int mxht = 0;
+        int ht = 0;
+        for(int i : gain){
+          ht = ht-i;
+          cout<<ht<<" ";
+          mxht = min(ht,mxht);
+        }
+       return (-mxht);
+    }
+};
