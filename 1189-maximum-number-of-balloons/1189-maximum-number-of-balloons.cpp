6917076dfaa56ef70ed0674ae3d44930c1c2384a
Avinash Bharti <avinashbharti3007@gmail.com>
2026-06-22 22:59:16 +0530

    Time: 3 ms (12.33%), Space: 9 MB (64.87%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        unordered_map<char,int>mp={
-           {'b',0},{'a',0},{'l',0},{'o',0},{'n',0}
-        };
-        int n = text.size();
-        vector<int>temp(5,0);
-        for(char s : text){
-            if(mp.find(s)!=mp.end()) mp[s]++;
+        unordered_map<char,int> mp;
+
+        for(char ch : text) {
+            mp[ch]++;
         }
 
-        for(auto &x : mp){
-            if(x.first=='b') temp[0]=x.second;
-            if(x.first=='a') temp[1]=x.second;
-            if(x.first=='l') temp[2]=x.second;
-            if(x.first=='o') temp[3]=x.second;
-            if(x.first=='n') temp[4]=x.second;
-        }
-        temp[2] /=2;
-        temp[3] /=2;
-        int mn = INT_MAX;
-        for(int i : temp) mn = min(mn,i);
-        return mn;
+        return min({
+            mp['b'],
+            mp['a'],
+            mp['l'] / 2,
+            mp['o'] / 2,
+            mp['n']
+        });
     }
 };
